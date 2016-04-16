9c466978c284584e189aca7257ad687004bff59c
Marvin Kuhn <marvin.kuhn92@gmail.com>
2016-04-16 19:17:56 +0200

    new api access class

--- src/KodiApi.js
+++ src/KodiApi.js
@@ -10,62 +10,81 @@ require('./KodiInputApi.js');
 require('./KodiSystemApi.js');
 require('./KodiVideoLibraryApi.js');
 
-function KodiApi(ip, port) {
+class KodiApi {
+  constructor(host, port) {
     var self = this;
-    this.connection = new KodiApiConnection(ip, port);
-    this.apis = [];
-}
 
-KodiApi.prototype.isApiInitiated = function(key, id) {
+    this.connection = new KodiApiConnection(host, port);
+    this.apis = {'playlist': [], 'player': [], 'addon': []};
+    
+    return function(key, id) {
+      key = key.toLowerCase();
+
+      if(!self.isApiInitiated(key, id))
+        self.initiateApi(key, id);
+
+      if(id != undefined)
+        return self.apis[key][id];
+
+      return self.apis[key];
+    }
+  }
+
+  isApiInitiated(key, id) {
     if(this.apis[key] == undefined)
-        return false;
+      return false;
     else if(id != undefined && this.apis[key][id] === undefined)
-        return false;
+      return false;
 
     return true;
-}
-KodiApi.prototype.get = function(key, id) {
-    key = key.toLowerCase();
+  }
 
-    if(!this.isApiInitiated(key, id))
-        this.initiateApi(key, id);
-
-    if(id != undefined)
-        return this.apis[key][id];
-
-    return this.apis[key];
-}
-KodiApi.prototype.initiateApi = function(key, id) {
+  initiateApi(key, id) {
+    console.log(this.apis);
     switch(key) {
-        case 'addon':
-            this.apis[key][id] = new KodiAddonApi(this.connection, id);
-            break;
-        case 'application':
-            this.apis[key] = new KodiApplicationApi(this.connection);
-            break;
-        case 'player':
-            this.apis[key][id] = new KodiPlayerApi(this.connection, id);
-            break;
-        case 'playlist':
-            this.apis[key][id] = new KodiPlaylistApi(this.connection, id);
-            break;
-        case 'audiolibrary':
-            this.apis[key] = new KodiAudioLibraryApi(this.connection);
-            break;
-        case 'videolibrary':
-            this.apis[key] = new KodiVideoLibraryApi(this.connection);
-            break;
-        case 'gui':
-            this.apis[key] = new KodiGuiApi(this.connection);
-            break;
-        case 'input':
-            this.apis[key] = new KodiGuiApi(this.connection);
-            break;
-        case 'system':
-            this.apis[key] = new KodiSystemApi(this.connection);
-            break;
-        default:
-            console.log('Error: Api not found ' + key);
+      case 'addon':
+        this.apis[key][id] = new KodiAddonApi(this.connection, id);
+        break;
+      case 'application':
+        this.apis[key] = new KodiApplicationApi(this.connection);
+        break;
+      case 'player':
+        this.apis[key][id] = new KodiPlayerApi(this.connection, id);
+        break;
+      case 'playlist':
+        this.apis[key][id] = new KodiPlaylistApi(this.connection, id);
+        break;
+      case 'audiolibrary':
+        this.apis[key] = new KodiAudioLibraryApi(this.connection);
+        break;
+      case 'videolibrary':
+        this.apis[key] = new KodiVideoLibraryApi(this.connection);
+        break;
+      case 'gui':
+        this.apis[key] = new KodiGuiApi(this.connection);
+        break;
+      case 'input':
+        this.apis[key] = new KodiGuiApi(this.connection);
+        break;
+      case 'system':
+        this.apis[key] = new KodiSystemApi(this.connection);
+        break;
+      default:
+        console.log('Error: Api not found ' + key);
     }
+  }
+
+  getActivePlayers() {
+    return KodiPlayerApi.getActivePlayers();
+  }
+
+  getAddons() {
+    return KodiAddonApi.getAddons();
+  }
+
+  getPlaylists() {
+    return KodiPlaylistApi.getPlaylists();
+  }
 }
+
 global.KodiApi = KodiApi;
